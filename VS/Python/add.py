4dda2abaaa3fda0d887e9c85a74f85b86d49c6f3
Колиберда Александр Дмитриевич <82934858+MrGreEe@users.noreply.github.com>
2021-09-22 08:23:59 +0300

    Update add.py

--- VS/Python/add.py
+++ VS/Python/add.py
@@ -1,2 +1,14 @@
-print('sdfsf'/t) # оставить вывод на текущей строке
-print('Hell'/n , 'sdfsdf') # перенести вывод на следующую строку 
+print('sdfsf\t') # оставить вывод на текущей строке
+print('Hell\n','sdfsdf') # перенести вывод на следующую строку 
+
+countries = (
+    ("Germany", 80.2, (("Berlin",3.326), ("Hamburg", 1.718))),
+    ("France", 66, (("Paris", 2.2),("Marsel", 1.6)))
+)
+ 
+for country in countries:
+    countryName, countryPopulation, cities = country
+    print("\nCountry: {}  population: {}".format(countryName, countryPopulation))
+    for city in cities:
+        cityName, cityPopulation = city
+        print("City: {}  population: {}".format(cityName, cityPopulation))
