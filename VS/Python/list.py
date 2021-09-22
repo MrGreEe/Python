8574605d20665d2a7d17c6c198708494b81a1503
Колиберда Александр Дмитриевич <82934858+MrGreEe@users.noreply.github.com>
2021-09-22 08:24:32 +0300

    Update list.py

--- VS/Python/list.py
+++ VS/Python/list.py
@@ -2,7 +2,8 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # Нужно вернуть список, который состоит из элементов, общих для этих двух списков.
-def big_list():
+
+def big_list(): # соединение двух массивов и сортировка
     #result = list(filter(lambda elem: elem in b, a))
     #result = [elem for elem in a if elem in b]
     #result = list(set(a) & set(b))
